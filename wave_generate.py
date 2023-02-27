bddd4bcd1ce70a1e0ae95483df40691cf10b0e3e
Yuchen Ouyang <ouyangyuchen619@gmail.com>
2023-02-27 21:01:51 +0800

    generalize parameters, add random to hamming window

--- wave_generate.py
+++ wave_generate.py
@@ -1,50 +1,41 @@
-import math
 import os.path
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy import signal
-import scipy
 
 # create configuration
-config = {'save_path': 'tags5_noise1.npy',
-          'time': 0.002, 'fs': 1000000, 'num': 5}
+save_path = 'tags5_noise1.npy'
+config = {'time': 0.004, 'fs': 10000000, 'num': 10}
+winlen = 6
+noise = 0.5     # standard deviation of gaussian noise
+edge_noise = 0.2
 
 # simulation time (sec) && sampling rate (Hz) && number of tags
 T, fs, num = config['time'], config['fs'], config['num']
 t = np.linspace(0, T, int(T * fs))
 
 # properties of square waveforms: frequency (Hz), amplitude, phase
-freq = np.arange(num) * 200 + 500
-amp = np.random.random(num) * 3 + 2 + 1j * (np.random.random(num) * 3 + 2)
-phase = np.random.random(num) * 2 * np.pi
-config['freq'] = freq
-config['amplitude'] = amp
+freq = np.arange(num) * 1500 + 3000
+amp = np.random.random(num) * 3 + 2 + 1j * (np.random.random(num) * 3 + 2)      # random amplitude
+phase = np.random.random(num) * 2 * np.pi       # random phase
+config['freq'] = freq       # add frequency to config
+config['amplitude'] = amp       # add amplitude to config
 
 # generate square waveforms
 result = 0
 for i in range(num):
     pwm = amp[i] * signal.square(2 * np.pi * freq[i] * t + phase[i])
-    window = signal.windows.hamming(math.floor(3))
+    window = signal.windows.hamming(winlen) + np.random.normal(0, edge_noise, winlen)
     pwm = signal.convolve(pwm, window, mode='same')
     result += pwm
 
-# plot result in time and frequency domain
-fig, axes = plt.subplots(2, 2, sharex='col')
-axes[0, 0].plot(abs(scipy.fft.fft(result)))
-axes[0, 0].set_title('No gaussian noise')
-
-axes[0, 1].plot(t, abs(result))
-
-result += np.random.normal(0, 0.5, len(t))
-axes[1, 0].plot(abs(scipy.fft.fft(result)))
-axes[1, 0].set_title('With gaussian noise')
-
-axes[1, 1].plot(t, abs(result))
-
+# add channel noise
+result += np.random.normal(0, noise, len(t))
+plt.plot(t, result)
 plt.show()
 
 # save wave to 'signals/'
-filename = os.path.join('signals', config['save_path'])
+filename = os.path.join('signals', save_path)
 with open(filename, 'wb') as f:
     np.save(f, config, allow_pickle=True)
     np.save(f, result)
