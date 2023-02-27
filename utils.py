110aeda8810c8c58cbc0c3b2d3614f2ee0bffe63
Yuchen Ouyang <ouyangyuchen619@gmail.com>
2023-02-27 21:01:03 +0800

    refactor loading and denoise into separate functions

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import os.path
 import numpy as np
-import scipy
 from scipy import signal
 from matplotlib import pyplot as plt
 
@@ -9,19 +8,25 @@ def loadWave(filename: str, lowpass: bool = False, window_size: int = 3):
     with open(filename, 'rb') as f:
         config = np.load(f, allow_pickle=True)
         result = np.load(f)
-    if lowpass:
-        hamming = signal.windows.hamming(window_size)
-        result = signal.convolve(result, hamming, mode='same')
     return config, result
 
 
+def deNoise(wave_arr: np.ndarray, window_size: int = 3):
+    wave_shift = np.roll(wave_arr, window_size)
+    wave_shift[:window_size] = 0.0
+    wave_arr -= wave_shift
+    hamming = signal.windows.hamming(window_size)
+    wave_arr = signal.convolve(wave_arr, hamming, mode='same')
+    return wave_arr[window_size:]
+
+
 if __name__ == '__main__':
+    fig, axes = plt.subplots(2, 1)
     wavefile = 'tags5_noise1.npy'
-    _, waveform = loadWave(os.path.join('signals', wavefile), lowpass=False)
-    fig, axes = plt.subplots(2, 1, sharey='col')
-    # axes[0, 0].plot(waveform)
-    axes[0].plot(waveform - np.roll(waveform, 3))
-    _, waveform = loadWave(os.path.join('signals', wavefile), window_size=5, lowpass=True)
-    # axes[1, 0].plot(waveform)
-    axes[1].plot(waveform - np.roll(waveform, 3))
+    _, wavearray = loadWave(os.path.join('signals', wavefile))
+    axes[0].plot(wavearray[100: 500])
+
+    wavearray = deNoise(wavearray, window_size=3)
+    axes[1].plot(wavearray[100: 500])
+
     plt.show()
