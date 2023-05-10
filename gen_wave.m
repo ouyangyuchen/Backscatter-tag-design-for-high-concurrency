058dd270ad0e01bf9fe6b5c89c40453ea5fa6eb8
Yuchen Ouyang <ouyangyuchen619@gmail.com>
2023-05-10 17:46:12 +0800

    Change snr from the aggregate signal to separate square waves

--- gen_wave.m
+++ gen_wave.m
@@ -1,16 +1,14 @@
 clear;
 %% Configurations
 % duration time, sampling freq, #tags
-time = 0.0002;
+time = 0.002;
 fs = 10000000;
-num = 20;
+num = 10;
 t = linspace(0, time, time * fs);
 
-winlen = 3;
-noise = 0.5;
-edge_noise = 0.2;
+SNR = 20;           % change this
 
-SNR = 40;
+SNR1 = 10 * log10(2 * num) + SNR;
 
 % all config/signal save in dir:
 if exist("signals/", 'dir') == 0
@@ -19,21 +17,24 @@ end
 
 %% Freqs, Phases, Amplitudes
 % 15kHz ~ 30kHz
-freq = (1:num) * 1500 + 30000;
-amp = (rand(1, num) * 5 + 5) .* exp(1j * 2*pi * rand(1, num));
+freq = rand(1, num) * 15000 + 15000;
+% amp = (rand(1, num) * .02 + .1) .* exp(1j * 2*pi * rand(1, num));
+amp = [0.105278488140166 - 0.022815703894954i,0.116970245460998 - 0.010401511833459i,-0.051829899302457 + 0.093509992767321i,-0.098740847394605 + 0.022109307227178i,-0.057775834523463 - 0.081945277760733i,0.026574569729239 - 0.104211139979910i,0.062621920169006 + 0.080339654927814i,0.044419656911533 + 0.097900755864330i,0.106761404818690 + 0.003799455470436i,-0.102182347104108 - 0.059953844562520i];
 phases = rand(1, num) * 2 * pi;
-% plot(real(amp), imag(amp), '.');
-
+plot(real(amp), imag(amp), 'o');
+hold on
+plot(-real(amp), -imag(amp), 'o')
+plot(0, 0, 'x');
+hold off
 %% Generate Wave
 wave = zeros(1, length(t));
 for i = 1:num
     pwm = amp(i) * square(2*pi * freq(i) * t + phases(i));
-    window = hamming(winlen) + edge_noise * randn(winlen, 1);
+    pwm = awgn(pwm, SNR1, 'measured');
+    window = 0.5 * hamming(3);
     pwm = conv(pwm, window, "same");
     wave = wave + pwm;
 end
-% gaussian noise to dB
-wave = awgn(wave, SNR, 'measured');
 % plot(t(1:800), wave(1:800));
 %hold off
 
