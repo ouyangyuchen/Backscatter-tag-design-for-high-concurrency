af657089831143785f567098c7efc2a8556336f1
Yuchen Ouyang <ouyangyuchen619@gmail.com>
2023-05-12 10:01:00 +0800

    Add get_freq and filtering to main.py

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ from viterbi_decoding import *
 from extract import ExtractPeaks
 
 # load wave
-file = 'signals/tags10_snr15_db.mat'
+file = 'signals/tags10_snr20_db.mat'
 ep = ExtractPeaks(filename=file, start=0, end=None)
 print("sigma \t %.4f" % ep.sigma)
 
 # extract edges from wave signal
 rx_signal = ep.extract(thres=0.1, duration=3)
-ep.plotEdges(rx_signal, 400)
+# ep.plotEdges(rx_signal, 400)
 
 # MAIN FUNCTION
 alpha = 338.56 * ep.sigma ** 4
 res = viterbi(rx_signal, alpha)
-num = filtering(rx_signal, res)
-print("tags: %d" % num)
+valid_class = filtering(rx_signal, res)
+print("tags: %d" % len(valid_class))
+
+# test performance
+freq = get_freq(rx_signal, res)
 
 # plot distribution density in the I-Q domain
 fig, axes = plt.subplots(1, 2, figsize=(8, 4))
